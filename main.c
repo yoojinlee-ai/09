c391902e40b99db1d3e1063b217696aa5bcb0a77
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-10-30 16:00:08 +0900

    9-3(1)

--- main.c
+++ main.c
@@ -9,24 +9,30 @@
 #define SIZE 5
 
 int main(int argc, char *argv[]) {
-    int testscore[SIZE];
-    int i;
-    int sum = 0;
-    float avg;
-
-    // 점수 입력 안내 출력
-    printf("%i명 점수를 입력하세요\n", SIZE);
-
-    for (i = 0; i < SIZE; i++) {
-        scanf("%d", &testscore[i]);   
-        sum += testscore[i];          // 오타 수정: teatscore -> testscore
-    }
-
-    avg = (float)sum / SIZE;
-
    
-    printf("평균: %.2f\n", avg);
-
+    int grade[SIZE];
+    int score[SIZE];
+    int i;
+    
+    
+    for (i = 0;i<SIZE;i++)
+    {
+    	grade[i] = rand()%100;
+	}
+	
+	for (i = 0;i<SIZE;i++)
+    {
+    	score[i] = grade[i];
+	}
+    
+    #if i
+    for (i = 0;i<SIZE;i++)
+        printf("grade[i%] = %i, score[%i] =")
+        
+    #endif
+    
+    system("PAUSE");
+   
     return 0;
 }
 
