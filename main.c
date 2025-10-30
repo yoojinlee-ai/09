ef241fae58ca8fe11717dfd1b3d0552c6ff42634
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-10-30 15:47:21 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+#include <stdio.h>
+#define SIZE 5
+
 int main(int argc, char *argv[]) {
-	
-	//5명의 학생에 대한 시험 성적 저장 배열 선언 
-	
-	int test_score[5];
-	int i;
-	int grade[5];
-	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
-	
-	//점수 화면 출력(반복문 활용)
-	
-	for (i=0;i<5;i++)
-       	printf("grade[%i] = %i\n", i, test_score[i]);
-	
-	system("PAUSE"); 
-	
-	return 0;
+    int testscore[SIZE];
+    int i;
+    int sum = 0;
+    float avg;
+
+    // 점수 입력 안내 출력
+    printf("%i명 점수를 입력하세요\n", SIZE);
+
+    for (i = 0; i < SIZE; i++) {
+        scanf("%d", &testscore[i]);   // & 추가 필요
+        sum += testscore[i];          // 오타 수정: teatscore -> testscore
+    }
+
+    avg = (float)sum / SIZE;
+
+    printf("총점: %d\n", sum);
+    printf("평균: %.2f\n", avg);
+
+    return 0;
 }
+
