7dd705022710122fcf3260f4cd195198239a071b
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-10-30 16:05:22 +0900

    9-3(2)

--- main.c
+++ main.c
@@ -1,38 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>  // system() 함수 선언 위해 필요
+
 #define SIZE 5
 
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-#include <stdio.h>
-#define SIZE 5
-
-int main(int argc, char *argv[]) {
-   
-    int grade[SIZE];
-    int score[SIZE];
+int main() {
     int i;
-    
-    
-    for (i = 0;i<SIZE;i++)
-    {
-    	grade[i] = rand()%100;
-	}
-	
-	for (i = 0;i<SIZE;i++)
-    {
-    	score[i] = grade[i];
-	}
-    
-    #if i
-    for (i = 0;i<SIZE;i++)
-        printf("grade[i%] = %i, score[%i] =")
-        
-    #endif
-    
+    int a[SIZE] = {1, 2, 3, 4, 5};  // () -> {} 로 변경
+    int b[SIZE] = {1, 5, 3, 4, 6};  // () -> {} 로 변경
+    int flag_same = 1;
+
+    for (i = 0; i < SIZE; i++) {
+        if (a[i] != b[i]) {
+            printf("%d-th element is diff.\n", i);  // printf 형식 수정, 변수 i 위치 수정
+            flag_same = 0;  // 다르면 flag 변경
+        }
+    }
+
+    printf("Are a and b same? %d\n", flag_same);
+
     system("PAUSE");
-   
+
     return 0;
 }
+
 
